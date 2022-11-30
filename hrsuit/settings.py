d3490dd4eea531b7dd3dfa8a476a28b0a25f14dc
shankar samidala <samidalashankar@outlook.com>
2022-11-30 20:19:45 +0530

    init

--- hrsuit/settings.py
+++ hrsuit/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'jhcbhjsckasduchlakcnkjfsakcjkxjkcbj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Email Settings
